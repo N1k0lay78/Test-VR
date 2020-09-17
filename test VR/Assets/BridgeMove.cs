cc6c2a1c61368b365a6a7c8dd9394c6efc0492a1
Rjkzavr <kolya.toropof@gmail.com>
2020-09-17 20:32:57 +0300

    Add alpha version attraction

---
+++ test VR/Assets/BridgeMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeMove : MonoBehaviour
+{
+    public float moveToX;
+    public float moveToZ;
+
+    private float heigth;
+
+    public bool isMove;
+
+    private Transform myTransfrom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransfrom = GetComponent<Transform>();
+        heigth = myTransfrom.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            isMove = !isMove;
+        }
+        if (isMove)
+        {
+            myTransfrom.position = new Vector3(moveToX, heigth, moveToZ);
+        }
+        else
+        {
+            myTransfrom.position = new Vector3(19, heigth, 14);
+        }
+    }
+}
